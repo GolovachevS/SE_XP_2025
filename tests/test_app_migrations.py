150b07e31f5c00b13a5e9ea9aab62e4b794e9ca9
Golovachev Sergey <golovacev905@gmail.com>
2025-10-22 17:56:50 +0300

    tests: add migrations tests

---
+++ tests/test_app_migrations.py
@@ -0,0 +1,53 @@
+import app as app_module
+
+
+def test_ensure_db_up_to_date_initializes_when_no_migrations(app, monkeypatch, tmp_path):
+    calls = {"init": 0, "migrate": 0, "upgrade": 0}
+
+    def fake_exists(path):
+        # Simulate that migrations folder does not exist
+        return False
+
+    def fake_init():
+        calls["init"] += 1
+
+    def fake_migrate(message=None):
+        calls["migrate"] += 1
+
+    def fake_upgrade():
+        calls["upgrade"] += 1
+
+    monkeypatch.setattr(app_module.os.path, "exists", fake_exists)
+    monkeypatch.setattr(app_module, "init", fake_init)
+    monkeypatch.setattr(app_module, "migrate_db", fake_migrate)
+    monkeypatch.setattr(app_module, "upgrade", fake_upgrade)
+
+    # Should call init + migrate + upgrade
+    app_module.ensure_db_up_to_date(app)
+    assert calls == {"init": 1, "migrate": 1, "upgrade": 1}
+
+
+def test_ensure_db_up_to_date_runs_migrate_and_upgrade_when_migrations_exist(app, monkeypatch):
+    calls = {"init": 0, "migrate": 0, "upgrade": 0}
+
+    def fake_exists(path):
+        # Simulate that migrations folder exists
+        return True
+
+    def fake_init():
+        calls["init"] += 1
+
+    def fake_migrate(message=None):
+        calls["migrate"] += 1
+
+    def fake_upgrade():
+        calls["upgrade"] += 1
+
+    monkeypatch.setattr(app_module.os.path, "exists", fake_exists)
+    monkeypatch.setattr(app_module, "init", fake_init)
+    monkeypatch.setattr(app_module, "migrate_db", fake_migrate)
+    monkeypatch.setattr(app_module, "upgrade", fake_upgrade)
+
+    # Should call migrate + upgrade only
+    app_module.ensure_db_up_to_date(app)
+    assert calls == {"init": 0, "migrate": 1, "upgrade": 1}
